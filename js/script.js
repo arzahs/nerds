8b96a0d49fe45b4616f1396dd44ff1f987a23d71
Sergey Nelepa <nelepa1995@mail.ru>
2015-07-14 22:44:57 +0300

    javascript for form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 var button = document.querySelector(".btn-mail");
 var form = document.querySelector(".modal-form-write");
+var exit = document.querySelector(".btn-close");
+var login = form.querySelector("[name=name]");
+var slidecontrols = document.querySelectorAll(".slider-controls i");
 
+/*slidecontrols.addEventListener("click", function(event){
+	event.preventDefault();
+	slidecontrols[0]
+}*/
 button.addEventListener("click", function(event){
 	event.preventDefault();
 	form.classList.add("modal-form-write-show");
+	login.focus();
 });
+
+exit.addEventListener("click", function(event){
+	event.preventDefault();
+	form.classList.remove("modal-form-write-show");
+});
+window.addEventListener("keydown", function(event){
+	if(event.keyCode == 27){
+		if(form.classList.contains("modal-form-write-show")){
+			form.classList.remove("modal-form-write-show");
+		}
+	}
+});
+
+/*var slideShow = function(event){
+	event.preventDefault();
+}*/
