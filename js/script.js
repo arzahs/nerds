7dba15d0ddc7997c3ebd2a657f568af570f347c8
Sergey Nelepa <nelepa1995@mail.ru>
2015-07-15 15:35:21 +0300

    yandex maps add

--- js/script.js
+++ js/script.js
@@ -8,6 +8,24 @@ var slide = document.querySelectorAll(".slide");
 	event.preventDefault();
 	slidecontrols[0]
 }*/
+button.addEventListener("click", function(event){
+	event.preventDefault();
+	form.classList.add("modal-form-write-show");
+	login.focus();
+});
+
+exit.addEventListener("click", function(event){
+	event.preventDefault();
+	form.classList.remove("modal-form-write-show");
+});
+window.addEventListener("keydown", function(event){
+	if(event.keyCode == 27){
+		if(form.classList.contains("modal-form-write-show")){
+			form.classList.remove("modal-form-write-show");
+		}
+	}
+});
+
 slidecontrols[0].addEventListener("click", function(event){
 	event.preventDefault();
 	if(slidecontrols[1].classList.contains("active")){
@@ -65,23 +83,6 @@ slidecontrols[2].addEventListener("click", function(event){
 	}
 	slidecontrols[2].classList.add("active");
 	slide[2].classList.add("slide-show");
-});
-button.addEventListener("click", function(event){
-	event.preventDefault();
-	form.classList.add("modal-form-write-show");
-	login.focus();
-});
-
-exit.addEventListener("click", function(event){
-	event.preventDefault();
-	form.classList.remove("modal-form-write-show");
-});
-window.addEventListener("keydown", function(event){
-	if(event.keyCode == 27){
-		if(form.classList.contains("modal-form-write-show")){
-			form.classList.remove("modal-form-write-show");
-		}
-	}
 });
 
 /*var slideShow = function(event){
